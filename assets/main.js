7f7cb1d63bdcacf2fcc9ece1bd27c6a34d2580d5
Isaac Barsoum <itbarsoum99@gmail.com>
2021-05-12 12:18:12 -0400

    first commit

---
+++ assets/main.js
@@ -0,0 +1,23 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("buttonactive");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
